55e8b9825c36e5b44af967a8cee3988918fdfd16
aras112 <aras112nowe@o2.pl>
2018-11-13 21:26:32 +0100

    jako tako dziala

--- app/src/main/java/com/example/aras1/myapplication/OpenActivity.java
+++ app/src/main/java/com/example/aras1/myapplication/OpenActivity.java
@@ -2,20 +2,21 @@ package com.example.aras1.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class OpenActivity extends AppCompatActivity
-{
+    {
 
     OpenActivityController controller;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
-    {
+        {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_collection);
-        controller=new OpenActivityController(this);
+        controller = new OpenActivityController(this);
+        }
+
+
     }
-
-
-}
--- app/src/main/java/com/example/aras1/myapplication/OpenActivityController.java
+++ app/src/main/java/com/example/aras1/myapplication/OpenActivityController.java
@@ -1,8 +1,5 @@
 package com.example.aras1.myapplication;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,7 +10,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OpenActivityController extends AppCompatActivity {
+public class OpenActivityController
+    {
 
     private OpenActivity activity;
     private TextView chooseCollectionLabel;
@@ -22,34 +20,43 @@ public class OpenActivityController extends AppCompatActivity {
     private List<FlashcardItem> flashcardList;
 
     OpenActivityController(OpenActivity activity)
-    {
+        {
         this.activity = activity;
-    }
+        init();
+        }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void init()
+        {
         chooseCollectionLabel = activity.findViewById(R.id.chooseCollectionLabel);
         collectionList = activity.findViewById(R.id.collectionList);
-
+        Log.i("plik ", "path");
         String path = Environment.getExternalStorageDirectory().getPath() + "/" + activity.getString(R.string.app_name);
         flashcardList = new ArrayList<>();
 
         File myDir = new File(path);
-        for (File f : myDir.listFiles()) {
-            if (f.isFile()) {
-                String name = f.getName();
-                flashcardList.add(new FlashcardItem(name,"01-01-1970"));
-                Log.i("kolekcja: ", name);
-            }
-        }
 
-        adapter = new FlashcardListAdapter(this,  flashcardList);
+        Log.i("plik ", myDir.getName());
+
+        for (File f : myDir.listFiles())
+            {
+            Log.i("plik ", f.getName());
+            if (f.isFile())
+                {
+                String name = f.getName();
+                Log.i("kolekcja: ", name);
+                flashcardList.add(new FlashcardItem(name, "01-01-1970"));
+                }
+            }
+
+
+        adapter = new FlashcardListAdapter(activity, flashcardList);
 
         collectionList.setAdapter(adapter);
 
         collectionList.setTextFilterEnabled(true);
 
-        collectionList.setOnItemClickListener((parent, view, position, id) -> {
+        collectionList.setOnItemClickListener((parent, view, position, id) ->
+        {
 //            Intent onTouchActivity = new Intent(MeetingsListActivity.this, CurrentMeetingActivity.class);
 //            onTouchActivity.putExtra("recorderName", recorderName);
 //            onTouchActivity.putExtra("Name", meetingItems.get(position).getName());
@@ -57,7 +64,6 @@ public class OpenActivityController extends AppCompatActivity {
         });
 
 
+        }
 
     }
-
-}
